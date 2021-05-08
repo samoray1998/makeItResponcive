2dbe05a8b73dadf5fdca227833838334dc678691
samoray1998 <jamalkoukou@outlook.com>
2021-05-08 01:27:56 +0000

    another commit

--- lib/Home.dart
+++ lib/Home.dart
@@ -7,76 +7,89 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-            child: Column(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      drawer: Drawer(child: Container() // Populate the Drawer in the next step.
+          ),
+      body: Container(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "Location de voiture au maroc",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 26.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "Choisissez votre destination",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Material(
-                elevation: 10.0,
-                borderRadius: BorderRadius.circular(30.0),
-                shadowColor: Color(0x55434343),
-                child: TextField(
-                  onChanged: (value) {
-                    // searchController.changeQuery(value.trim());
-                  },
-                  textAlign: TextAlign.start,
-                  textAlignVertical: TextAlignVertical.center,
-                  decoration: InputDecoration(
-                    hintText: "Rechercher une ville ...",
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: Colors.black54,
+            Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    "Location de voiture au maroc",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.w600,
                     ),
-                    border: InputBorder.none,
                   ),
                 ),
-              ),
+                SizedBox(
+                  height: 5,
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    "Choisissez votre destination",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Material(
+                    elevation: 10.0,
+                    borderRadius: BorderRadius.circular(30.0),
+                    shadowColor: Color(0x55434343),
+                    child: TextField(
+                      onChanged: (value) {
+                        // searchController.changeQuery(value.trim());
+                      },
+                      textAlign: TextAlign.start,
+                      textAlignVertical: TextAlignVertical.center,
+                      decoration: InputDecoration(
+                        hintText: "Rechercher une ville ...",
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black54,
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30.0),
+              ],
             ),
-            SizedBox(height: 30.0),
-            DefaultTabController(
-                length: 3,
-                child: Column(
-                  children: [
-                    Container(
-                      child: TabBar(
+            Expanded(
+              child: Container(
+                //height: double.infinity,
+                child: DefaultTabController(
+                  length: 3,
+                  // child: Expanded(
+                  child: Column(
+                    children: [
+                      TabBar(
                         indicatorColor: Color(0xFFFE8C68),
                         unselectedLabelColor: Color(0xFF555555),
                         labelColor: Color(0xFFFE8C68),
-                        labelPadding: EdgeInsets.symmetric(horizontal: 8.0),
+                        //labelPadding: EdgeInsets.symmetric(horizontal: 8.0),
                         tabs: [
                           Tab(
                             text: "Villes",
@@ -92,27 +105,37 @@ class Home extends StatelessWidget {
                           // ),
                         ],
                       ),
-                    ),
-                    Container(
-                      height: size.height * 0.64,
-                      child: TabBarView(
-                        children: [
-                          Container(
-                            color: Colors.blue,
-                          ),
-                          Container(
-                            color: Colors.red,
-                          ),
-                          Container(
-                            color: Colors.black,
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ))
+                      Expanded(
+                          child: Container(
+                        //height: 500,
+                        child: TabBarView(
+                          children: [
+                            //Now let's create our first tab page
+                            Container(
+                              color: Colors.red,
+                            ),
+                            Container(
+                              color: Colors.blue,
+                            ),
+                            Container(
+                              color: Colors.green,
+                            ),
+                            // Container(
+                            //   child: ListView(
+                            //     scrollDirection: Axis.horizontal,
+                            //     children: [],
+                            //   ),
+                            // ),
+                          ],
+                        ),
+                      )),
+                    ],
+                  ),
+                ),
+              ),
+            )
           ],
-        )),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:make_app_responcive/Home.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(MyApp());
 }
 
